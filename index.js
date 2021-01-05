3153abc3d0fb3bace1106fa0f55bdf8cc7ccb73d
rzmsv <rmussavi@gmail.com>
2021-01-05 23:34:17 +0330

    error 404 handler

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const homePage = require("./routess/home");
 const adminPage = require("./routess/admin");
 const internsPage = require("./routess/interns");
+const errorPages = require("./routess/error")
 
 const app = express();
 const server = http.createServer(app);
@@ -19,8 +20,10 @@ app.use(bodyParser.urlencoded({extended:false}));
 //routes
 app.use("/",homePage);
 app.use(adminPage);
-app.use("/interns",internsPage);
+app.use("/intern/:id",internsPage);
 
+// error pages
+app.use(errorPages.get404)
 
 const port = process.env.PORT || 3000;
 server.listen(port,()=>{
---
+++ routess/error.js
@@ -0,0 +1,3 @@
+module.exports.get404 = (req,res,next)=>{
+     res.send("<h1> ERROR 404 </h1>")
+}
